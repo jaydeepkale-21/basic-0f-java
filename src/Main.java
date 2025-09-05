14545d5884f9655ae7f36215f30595f0b6ad4d41
jaydeepkale-21 <kalejaydeep21@email.com>
2025-09-05 13:43:21 +0530

    looping conpect

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
@@ -31,16 +33,66 @@ public class Main {
 //        System.out.println("after swaping:"+a+":"+b);
 
 //        Swapping using "XOR-Opertor"
-        int a=78;
-        int b=12;
-        System.out.println("befor swaping:"+a+":"+b);
-        a=a^b;
-        b=a^b;
-        a=a^b;
-        System.out.println("after swaping:"+a+":"+b);
+//        int a=78;
+//        int b=12;
+//        System.out.println("befor swaping:"+a+":"+b);
+//        a=a^b;
+//        b=a^b;
+//        a=a^b;
+//        System.out.println("after swaping:"+a+":"+b);
 
 
+//--------------------------type casting---------------------//
+        //------(i)implicit(widenning) type cast
+//        -automatic type cast  bi compiler
+//        int a=24;
+//        float b=a;
+//        System.out.println("implicit type cast:"+b);
+//        int result=5;
+//        for (int i=1;i<11;i++){
+//          ;
+//            System.out.println(result*i);
+//
+//        }
 
+//        loop in java
+//        for (int i=1;i<8;i++){
+//            System.out.println("hefshine");
+//        }
+        //1<8  true
+        //2<8  true
+        //3<8  true
+        //4<8  true
+        //5<8  true
+        //6<8  true
+        //7<8  true
+        //8<8  false
 
+        //2 table printing
+        int num;
+        System.out.println("enter number:");
+        Scanner sc= new Scanner(System.in);
+        num=sc.nextInt();
+            for (int i=1;i<11;i++){
+                System.out.println(num+ " * " + i +" = "+ (num*i));
+            }
+
+//        Even odd number printing
+//        System.out.print("even = ");
+//        for (int i = 1; i <= 10; i++) {
+//            if (i % 2 == 0) {
+//                System.out.print( i + " ");
+//
+//            }
+//
+//        }
+//        System.out.println();
+//        System.out.print("odd = ");
+//        for (int i = 1; i <= 10; i++) {
+//            if (i % 2 != 0) {
+//                System.out.print( i + " ");
+//            }
+//
+//        }
     }
-}
+    }
