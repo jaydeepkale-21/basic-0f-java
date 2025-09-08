bcfc6dcfa4de6d121f17d6dae0078ddcdad62c4b
jaydeepkale-21 <kalejaydeep21@email.com>
2025-09-08 13:31:04 +0530

    9/8/25

--- src/Main.java
+++ src/Main.java
@@ -4,22 +4,48 @@ import java.util.Scanner;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-//          condition statement
-    int a;
-    Scanner sc=new Scanner(System.in);
-    System.out.println("enter a:");
-    a=sc.nextInt();
-//    System.out.println("enter b");
-//    int b=sc.nextInt();
-    if(99<a && 1000>a){
-        System.out.println("number is three digit");
-    }
-//    else if(a<b){
-//        System.out.println("numbre b is greater");
+        //sum of number 1 To 10
+//        int sum=0;
+//    for (int i=1;i<=10;i++){
+//      sum+=i;
 //    }
-    else{
-        System.out.println("number is not three digit");
-    }
+//        System.out.println("sum of 1 to 100 is:" + sum);
+
+//        factorical of number
+//        int num;
+//        Scanner sc=new Scanner(System.in);
+//        System.out.println("enter any to find factorical:");
+//        num=sc.nextInt();
+//        int fact=1;
+//        for (int i=1;i<=num;i++){
+//            fact*=i;
+//            //fact=fact*i;
+////            1*1=1
+////            1*2=2
+////            2*3=6
+////            6*4=24
+////            24*5=120
+//
+//        }
+//        System.out.println("factorical value is:"+ fact);
+
+//        odd even addtion
+        int num;
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter any number:");
+        num=sc.nextInt();
+        int even_sum=0;
+        int odd_sum=0;
+        for (int i=1;i<=num;i++){
+            if (i%2==0){
+                even_sum+=i;
+            }
+            else {
+                odd_sum+=i;
+            }
+        }
+        System.out.println("sum of even number:"+even_sum);
+        System.out.println("sum od odd number:"+odd_sum);
 
 
     }
