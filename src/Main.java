1f26deb9bda6d5fa1b7c11c417fb216aafd27ca6
jaydeepkale-21 <kalejaydeep21@email.com>
2025-09-08 11:49:36 +0530

    new conspt

--- src/Main.java
+++ src/Main.java
@@ -4,95 +4,23 @@ import java.util.Scanner;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-
-//        swaping of two number using third variable
-//        int a=38;
-//        int b=54;
-//        System.out.println("befor swaping:"+a+":"+b);
-//        int temp=a;
-//        a=b;
-//        b=temp;
-//        System.out.println("after swaping:"+a+":"+b);
-
-//        swaping using arithmetic opertor "+","-"
-//        int a=73;
-//        int b=36;
-//        System.out.println("befor swaping:"+a+":"+b);
-//        a=a+b;
-//        b=a-b;
-//        a=a-b;
-//        System.out.println("after swaping:"+a+":"+b);
-
-//        Using "*","/"
-//        int a=48;
-//        int b=90;
-//        System.out.println("befor swaping:"+a+":"+b);
-//        a=a*b;
-//        b=a/b;
-//        a=a/b;
-//        System.out.println("after swaping:"+a+":"+b);
-
-//        Swapping using "XOR-Opertor"
-//        int a=78;
-//        int b=12;
-//        System.out.println("befor swaping:"+a+":"+b);
-//        a=a^b;
-//        b=a^b;
-//        a=a^b;
-//        System.out.println("after swaping:"+a+":"+b);
+//          condition statement
+    int a;
+    Scanner sc=new Scanner(System.in);
+    System.out.println("enter a:");
+    a=sc.nextInt();
+//    System.out.println("enter b");
+//    int b=sc.nextInt();
+    if(99<a && 1000>a){
+        System.out.println("number is three digit");
+    }
+//    else if(a<b){
+//        System.out.println("numbre b is greater");
+//    }
+    else{
+        System.out.println("number is not three digit");
+    }
 
 
-//--------------------------type casting---------------------//
-        //------(i)implicit(widenning) type cast
-//        -automatic type cast  bi compiler
-//        int a=24;
-//        float b=a;
-//        System.out.println("implicit type cast:"+b);
-//        int result=5;
-//        for (int i=1;i<11;i++){
-//          ;
-//            System.out.println(result*i);
-//
-//        }
-
-//        loop in java
-//        for (int i=1;i<8;i++){
-//            System.out.println("hefshine");
-//        }
-        //1<8  true
-        //2<8  true
-        //3<8  true
-        //4<8  true
-        //5<8  true
-        //6<8  true
-        //7<8  true
-        //8<8  false
-
-        //2 table printing
-//        int num;
-//        System.out.println("enter number:");
-//        Scanner sc= new Scanner(System.in);
-//        num=sc.nextInt();
-//            for (int i=1;i<11;i++){
-//                System.out.println(num+ " * " + i +" = "+ (num*i));
-//            }
-
-//        Even odd number printing
-//        System.out.print("even = ");
-//        for (int i = 1; i <= 10; i++) {
-//            if (i % 2 == 0) {
-//                System.out.print( i + " ");
-//
-//            }
-//
-//        }
-//        System.out.println();
-//        System.out.print("odd = ");
-//        for (int i = 1; i <= 10; i++) {
-//            if (i % 2 != 0) {
-//                System.out.print( i + " ");
-//            }
-//
-//        }
     }
     }
