b0cee1dc63f27893c1a49460533f7b63b66b2b5d
jaydeepkale-21 <kalejaydeep21@email.com>
2025-09-10 11:31:49 +0530

    looping and switch case

--- src/Main.java
+++ src/Main.java
@@ -30,23 +30,81 @@ public class Main {
 //        System.out.println("factorical value is:"+ fact);
 
 //        odd even addtion
-        int num;
-        Scanner sc=new Scanner(System.in);
-        System.out.println("enter any number:");
-        num=sc.nextInt();
-        int even_sum=0;
-        int odd_sum=0;
-        for (int i=1;i<=num;i++){
-            if (i%2==0){
-                even_sum+=i;
-            }
-            else {
-                odd_sum+=i;
-            }
-        }
-        System.out.println("sum of even number:"+even_sum);
-        System.out.println("sum od odd number:"+odd_sum);
+//        int num;
+//        Scanner sc=new Scanner(System.in);
+//        System.out.println("enter any number:");
+//        num=sc.nextInt();
+//        int even_sum=0;
+//        int odd_sum=0;
+//        for (int i=1;i<=num;i++){
+//            if (i%2==0){
+//                even_sum+=i;
+//            }
+//            else {
+//                odd_sum+=i;
+//            }
+//        }
+//        System.out.println("sum of even number:"+even_sum);
+//        System.out.println("sum od odd number:"+odd_sum);
 
+        int a;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter a date:");
+        a = sc.nextInt();
+//        if (90<=a && 100>=a){
+//            System.out.println("grade is 'a':"+a );
+//        } else if (80<=a && 90>=a) {
+//            System.out.println("grade is 'b':"+a);
+//        }
+//        else if (75<=a && 80>=a){
+//            System.out.println("grade is 'b':"+a);
+//        } else if (50<=a && 75>=a) {
+//            System.out.println("grade is 'd':"+a);
+//        }
+//        else {
+//            System.out.println("fail");
+//        }
+//        if (a == 1) {
+//            System.out.println("monday");
+//        } else if (a == 2) {
+//            System.out.println("tuesday");
+//        } else if (a == 3) {
+//            System.out.println("wednesday");
+//        } else if (a == 4) {
+//            System.out.println("thursday");
+//        } else if (a == 5) {
+//            System.out.println("friday");
+//        } else if (a == 6) {
+//            System.out.println("saturday");
+//        } else if (a == 7) {
+//            System.out.println("sunday");
+//        } else {
+//            System.out.println("invalid");
+//        }
+//        switch (a) {
+//            case 1:
+//                System.out.println("monday");
+//                break;
+//
+//            case 2:
+//                System.out.println("tuesday");
+//                break;
+//
+//            case 3:
+//                System.out.println("wednesday");
+//                break;
+//            case 4:
+//                System.out.println("thursday");
+//                break;
+//            case 5:
+//                System.out.println("friday");
+//                break;
+//            case 6:
+//                System.out.println("staurday");
+//                break;
+//            case 7:
+//                System.out.println("SUNDAY");
+//        }
 
     }
-    }
+}
