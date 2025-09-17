55ef0579aff84f5e99f9ade6cc41af09cfafcdc5
jaydeepkale-21 <kalejaydeep21@email.com>
2025-09-17 11:37:25 +0530

    patten

--- src/Main.java
+++ src/Main.java
@@ -4,171 +4,76 @@ import java.util.Scanner;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-//square printing
-//    for (int i=1;i<=5;i++){//i=1 i=2 i=3
-//        for (int j=1;j<=1;j++){         //j=1 j=2 j=3 j=4 j=5
-//            System.out.print(" * ");      //j=1 j=2 j=3 j=4 j=5
-//        }                               //j=1 j=2 j=3 j=4 j=5
-//        System.out.println();
-//    }
 
-//        Perfect triangle
-//        for (int i=1;i<=5;i++) {
-//            for (int j = i; j <= 5; j++) {
-//                System.out.print(" ");
-//            }
-//            for(int k=1;k<=i;k++){
-//                System.out.print("* ");
-//            }
-//            System.out.println();
-//
-//        }
-
-//        another method to printing left angle
-//        for (int i=5;i>=1;i--){
-//            for (int j=i;j>=1;j--){
-//                System.out.print(" * ");
-//            }
-//            System.out.println();
-//        }
-
-
-//        Today
-
-//        int num=1234;
-//        System.out.println(num%10);//5
-//        int num1=num/10;
-//        System.out.println(num1%10);//4
-//        int num2=num1/10;
-//        System.out.println(num2%10);
-//        int num3=num2/10;
-////        System.out.println(num3);
-//
-//        int i=1;
-//        while (i<=10){
-//            System.out.println(i);
-//            i++;
-//        }
-//        int i=10;
-//        while (i>=1){
-//            System.out.println(i);
-//            i--;
-//        }
-//
-//        int i=1;
-//        int num=5;
-//        while (i<=10){
-//            System.out.println(i*5);
-//            i++;
-//        }
-//
-//        int num;
-//        Scanner sc=new Scanner(System.in);
-//        System.out.println("enter a number and give it reveser:");
-//        num=sc.nextInt();
-//        int rev=0;
-//        while (num>0){//num=1234 123 12 1 0
-//            rev=num%10;
-//            System.out.print(rev);//rev=4 3 2 1
-//            num/=10;
-//        }
-
-
-//        for (int i = 1; i <= 5; i++) {// i=1
-//            for (int j = i; j <= 5; j++) {//j=1 j<=i=1
-//                System.out.print(" ");
-//            }
-//            for (int k = 1; k <= 5; k++) {
-//                System.out.print("* ");
-//
-//            }
-//            System.out.println();
-//
-//        }
-//for (int i=1;i<=5;i++){// i=1
-//    for (int j=1;j<=i;j++){//j=1 j<=i=1
-//        System.out.print(" ");
-//    }
-//    for (int k=1;k<=5;k++){
-//        System.out.print("* ");
-//
-//    }
-//    System.out.println();
-//
-//}
-//
-//        for (int i = 1; i <= 5; i++) {// i=1
-//            for (int j = 1; j <= i; j++) {//j=1 j<=i=1
-//                System.out.print(" ");
-//            }
-//            for (int k = 1; k <= 5; k++) {
-//                System.out.print("* ");
-//
-//            }
-//            System.out.println();
-//
-//        }
-//        for (int i=1;i<=5;i++){// i=1
-//            for (int j=i;j<=5;j++){//j=1 j<=i=1
+//        upper  design
+//        for (int i=1;i<=5;i++){
+//            for (int j=1;j<=i;j++){
 //                System.out.print(" ");
 //            }
 //            for (int k=1;k<=5;k++){
-//                System.out.print("* ");
-//
+//                System.out.print("*");
+//            }
+//            for (int r=i;r<=5;r++){
+//                System.out.print(" ");
+//            }
+//            for (int h=i;h<=5;h++){
+//                System.out.print(" ");
+//            }
+//            for (int w=1;w<=5;w++){
+//                System.out.print("*");
 //            }
 //            System.out.println();
+//        }
 //
+////        lower degin
+//    for (int i=1;i<=5;i++){
+//        for (int k=i;k<=5;k++){
+//            System.out.print(" ");
+//        }
+//        for (int j=1;j<=5;j++){
+//            System.out.print("*");
+//        }
+//        for (int f=1;f<=i;f++){
+//            System.out.print(" ");
+//        }
+//        for (int s=1;s<=i;s++){
+//            System.out.print(" ");
+//        }
+//        for (int e=1;e<=5;e++){
+//            System.out.print("*");
+//        }
+//        System.out.println();
+//    }
+
+
+//
+//        for (int i=1;i<=10;i++){
+//            for (int j=1;j<=i*2-1;j++){
+//                System.out.print("*");
+//            }
+//            System.out.println();
 //        }
 
-
-//                int n = 5;
 //
-//                // Top half
-//                for (int i = 1; i <= n; i++) {
-//                    // Left side ( > shift )
-//                    for (int j = 1; j <= i; j++) {
-//                        System.out.print(" ");
-//                    }
-//                    for (int k = 1; k <= n; k++) {
-//                        System.out.print("*");
-//                    }
-//
-//                    // Space between two parts
-//                    for (int s = 1; s <= (n - i) * 2; s++) {
-//                        System.out.print(" ");
-//                    }
-//
-//                    // Right side ( < shift )
-//                    for (int k = 1; k <= n; k++) {
-//                        System.out.print("*");
-//                    }
-//
-//                    System.out.println();
-//                }
-//
-//                // Bottom half
-//                for (int i = n; i >= 1; i--) {
-//                    // Left side ( > shift )
-//                    for (int j = 1; j <= i; j++) {
-//                        System.out.print(" ");
-//                    }
-//                    for (int k = 1; k <= n; k++) {
-//                        System.out.print("*");
-//                    }
-//
-//                    // Space between two parts
-//                    for (int s = 1; s <= (n - i) * 2; s++) {
-//                        System.out.print(" ");
-//                    }
-//
-//                    // Right side ( < shift )
-//                    for (int k = 1; k <= n; k++) {
-//                        System.out.print("*");
-//                    }
-//
-//                    System.out.println();
-//                }
+        for (int i=1;i<=5;i++){
+            for (int j=i;j<=5;j++){
+                System.out.print(" ");
+            }
+            for (int k=1;k<=i*2-1;k++){
+                System.out.print("*");
+            }
+            System.out.println();
         }
+        for (int i=1;i<=5;i++){
+            for (int j=1;j<=i;j++){
+                System.out.print(" ");
+            }
+            for (int k=i*2-1;k<=9;k++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
     }
 
 
