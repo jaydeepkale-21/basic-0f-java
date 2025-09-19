17cd6e610e00d614436b3f7951152924694b9eb6
jaydeepkale-21 <kalejaydeep21@email.com>
2025-09-19 11:31:43 +0530

    while conditon

--- src/Main.java
+++ src/Main.java
@@ -1,79 +1,40 @@
 import java.util.Scanner;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-
-//        upper  design
-//        for (int i=1;i<=5;i++){
-//            for (int j=1;j<=i;j++){
-//                System.out.print(" ");
-//            }
-//            for (int k=1;k<=5;k++){
-//                System.out.print("*");
-//            }
-//            for (int r=i;r<=5;r++){
-//                System.out.print(" ");
-//            }
-//            for (int h=i;h<=5;h++){
-//                System.out.print(" ");
-//            }
-//            for (int w=1;w<=5;w++){
-//                System.out.print("*");
-//            }
-//            System.out.println();
-//        }
+    Scanner sc=new Scanner(System.in);
+        System.out.println("enter a number:");
+        int a=sc.nextInt();
+        int sum=0;
+        int temp=a;
+//        while(a>0){
+//            int rem=a%10;
+//            sum=sum+rem;
+//            a/=10;
 //
-////        lower degin
-//    for (int i=1;i<=5;i++){
-//        for (int k=i;k<=5;k++){
-//            System.out.print(" ");
 //        }
-//        for (int j=1;j<=5;j++){
-//            System.out.print("*");
-//        }
-//        for (int f=1;f<=i;f++){
-//            System.out.print(" ");
-//        }
-//        for (int s=1;s<=i;s++){
-//            System.out.print(" ");
-//        }
-//        for (int e=1;e<=5;e++){
-//            System.out.print("*");
-//        }
-//        System.out.println();
-//    }
+//      if(temp%sum==0){
+//          System.out.println("Harshad number");
+//      }
+//      else{
+//          System.out.println("not hash");
+//      }
 
+//        ------> Pailndrome number <---------
+        int rev=0;
+        while(a>0){        //121>0     //12>0       //1>0           //0>0 false
+            int rem=a%10;  //121%10=1  //12%10=2    //1%10=1
+            rev=rev*10+rem;//0*10+1=1  //1*10+2=12  //12*10+1=121
+            a/=10;         //121/10=12 //12/10=1    //1/10=0
 
-//
-//        for (int i=1;i<=10;i++){
-//            for (int j=1;j<=i*2-1;j++){
-//                System.out.print("*");
-//            }
-//            System.out.println();
-//        }
-
-//
-        for (int i=1;i<=5;i++){
-            for (int j=i;j<=5;j++){
-                System.out.print(" ");
-            }
-            for (int k=1;k<=i*2-1;k++){
-                System.out.print("*");
-            }
-            System.out.println();
         }
-        for (int i=1;i<=5;i++){
-            for (int j=1;j<=i;j++){
-                System.out.print(" ");
-            }
-            for (int k=i*2-1;k<=9;k++){
-                System.out.print("*");
-            }
-            System.out.println();
+        if (temp==rev){ //121==121-true
+            System.out.println("number is palindrome");
         }
-    }
-    }
+        else {
+            System.out.println("not palindrome");
+        }
+
+    }}
 
 
