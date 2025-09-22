f8bcd738d5f199e26ff9ff45b85a916cf75d6e0f
jaydeepkale-21 <kalejaydeep21@email.com>
2025-09-22 11:47:00 +0530

    do while loop

--- src/Main.java
+++ src/Main.java
@@ -1,40 +1,86 @@
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-    Scanner sc=new Scanner(System.in);
-        System.out.println("enter a number:");
-        int a=sc.nextInt();
-        int sum=0;
-        int temp=a;
-//        while(a>0){
-//            int rem=a%10;
-//            sum=sum+rem;
-//            a/=10;
+
+// int j=1;
+// do {
+//     System.out.println(j);
+//     i++;
+// }while (j<=10);
+
+//        10 to 1
+//        int k=10;
+//        do {
+//            System.out.println(k);
+//            k--;
+//        }while (k>=1);
+
+
+//        printing table 5
+//        Scanner sc=new Scanner(System.in);
+//        int i=1;
+//        System.out.println("Enter a number to give it's table:");
+//        int num=sc.nextInt();
+//        do {
+//            System.out.println(i+" * 5: "+(i*num));
+//            i++;
+//        }while (i<=10);
+
+
+
+//        switch and do while
+//        Scanner sc=new Scanner(System.in);
+//        System.out.println("Enter the number:");
+//        int num=sc.nextInt();
+//        int ch;
+//        do {
+//            System.out.println("1.Even_odd, 2Positive_negative, 3.exit");
+//            System.out.println("Enter your choose:");
+//            ch = sc.nextInt();
+//            switch(ch) {
+//                case 1:
+//                    if (num % 2 == 0) {
+//                        System.out.println("Even number");
+//                    } else {
+//                        System.out.println("odd number");
+//                    }
+//                    break;
+//                case 2:
+//                    if (num > 0) {
+//                        System.out.println("number is positive");
+//                    } else if (num < 0) {
+//                        System.out.println("number is negative");
+//                    } else {
+//                        System.out.println("number is zero");
+//                    }
+//                    break;
 //
+//                case 3:
+//                    System.out.println("Exit");
+//                    break;
+//                default:
+//                    System.out.println("Invalid");
+//            }
 //        }
-//      if(temp%sum==0){
-//          System.out.println("Harshad number");
-//      }
-//      else{
-//          System.out.println("not hash");
-//      }
+//            while(ch!=3);
 
-//        ------> Pailndrome number <---------
-        int rev=0;
-        while(a>0){        //121>0     //12>0       //1>0           //0>0 false
-            int rem=a%10;  //121%10=1  //12%10=2    //1%10=1
-            rev=rev*10+rem;//0*10+1=1  //1*10+2=12  //12*10+1=121
-            a/=10;         //121/10=12 //12/10=1    //1/10=0
-
-        }
-        if (temp==rev){ //121==121-true
-            System.out.println("number is palindrome");
-        }
-        else {
-            System.out.println("not palindrome");
+        for (int i=65;i<=69;i++){
+            for (int j=i;j<=69;j++){
+                System.out.print(" ");
+            }
+            for (int k=65;k<=i;k++){
+                if(i==65 || k==i) {
+                    System.out.print((char)65);
+                }
+                else {
+                    System.out.print((char)k+" ");
+                }
+            }
+            System.out.println();
         }
 
-    }}
+    }
+}
 
 
