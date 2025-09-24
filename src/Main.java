642a4c10f4341fb561afb3cc8f751e13fac89c0f
jaydeepkale-21 <kalejaydeep21@email.com>
2025-09-24 09:16:23 +0530

    patten

--- src/Main.java
+++ src/Main.java
@@ -53,12 +53,12 @@ public class Main {
 //            System.out.println();
 //        }
 
-        for (int i=1;i<=5;i++){
-            for (int j=i;j>=1;j--){
-                System.out.print(j+" ");
-            }
-            System.out.println();
-        }
+//        for (int i=1;i<=5;i++){
+//            for (int j=i;j>=1;j--){
+//                System.out.print(j+" ");
+//            }
+//            System.out.println();
+//        }
 //        Output:
 //        1
 //        21
