b18caeadf37a83d55fe3795bc23ffadd4be22e1f
jaydeepkale-21 <kalejaydeep21@email.com>
2025-09-24 09:15:37 +0530

    some patten

--- src/Main.java
+++ src/Main.java
@@ -2,17 +2,71 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        Student n= new Student();
-        System.out.println("Name is: "+n.name);
-        System.out.println("age is: "+n.age);
-        System.out.println("Height is: "+n.height);
+//    for(int i=1;i<=5;i++){
+//        for (int j=1;j<i;j++){
+//            System.out.print(" ");
+//        }
+//        for (int k=1;k<=5;k++){
+//            System.out.print("*");
+//        }
+//        for (int l=1;l<=(5-i)*2;l++){
+//            System.out.print(" ");
+//        }
+//        for (int h=1;h<=5;h++){
+//            System.out.print("*");
+//        }
+//        System.out.println();
+//    }
+//        for(int i=4;i>=1;i--){
+//            for (int j=1;j<i;j++){
+//                System.out.print(" ");
+//            }
+//            for (int k=1;k<=5;k++){
+//                System.out.print("*");
+//            }
+//            for (int l=1;l<=(5-i)*2;l++){
+//                System.out.print(" ");
+//            }
+//            for (int h=1;h<=5;h++){
+//                System.out.print("*");
+//            }
+//            System.out.println();
+//        }
+//        Output :
+// *****        *****
+// *****      *****
+//  *****    *****
+//   *****  *****
+//    **********
+//   *****  *****
+//  *****    *****
+// *****      *****
+//*****        *****
 
+//    int count=0;
+//        for(int i=1;i<=5;i++){
+//            for(int j=1;j<=i;j++){
+//                count++;
+//                System.out.print(count+" ");
+//            }
+//
+//            System.out.println();
+//        }
+
+        for (int i=1;i<=5;i++){
+            for (int j=i;j>=1;j--){
+                System.out.print(j+" ");
+            }
+            System.out.println();
+        }
+//        Output:
+//        1
+//        21
+//        321
+//        4321
+//        54321
     }
-}
-class Student{
-    String name="Jaydeep";
-    int age=20;
-    float height=5.8f;
 
 }
+
 
