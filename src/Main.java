5aa618e200d5b8a1facd63dad377502ec413a7f1
jaydeepkale-21 <kalejaydeep21@email.com>
2025-10-03 09:47:13 +0530

    all data printing

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,36 @@
 import java.util.*;
 public class Main {
     public static void main(String[] args) {
+    techer t1=new techer();
 
-
+    t1.getvalue((byteyu)10);
     }
 }
+//class student{
+//    String n;
+//    int a;
+//    long p;
+//    student(String name, int id, long pin){
+//        n=name;
+//        a=id;
+//        p=pin;
+//    }
+//    student(student s1){
+//        n=s1.n;
+//        a=s1.a;
+//        p=s1.p;
+//    }
+//    void show(){
+//        System.out.println("name: "+n+" id: "+a+" pin: "+p);
+//    }
+//}
 
-
+//   -------------> Inhristance <------------------
+class techer{
+void getvalue(byte a){
+    System.out.println("print valua:"+a);
+}
+}
 
 
 
--- src/Oops_java/oop_concept_1.java
+++ src/Oops_java/oop_concept_1.java
@@ -1,44 +1,87 @@
 package Oops_java;
 
 import java.util.Scanner;
-
-public class oop_concept_1 {
-    public static void main(String[] args){
-        demo t1=new demo();
-        t1.get();
-        t1.get(42);
-        t1.get(34f);
-
-    }
-}
-//class demo{
-//    void num(float n){
-//        System.out.println(n);
+//class Static_overrding{
+//    static void get(int a){
+//        System.out.println("outer: "+a);
 //    }
-//    void name(int age,String name){
-//        System.out.println(age+" "+name);
+//    static void get(float b){
+//        System.out.println("outer: "+b);
 //    }
-//    void num1(double i,int j){
-//        System.out.println(i+" "+j);
+//    static void get(String name){
+//        System.out.println("outer: "+name);
 //    }
 //}
-//class test1 {
-//    void get(int age, String name) {
-//        System.out.println(name+" age is: "+age);
+
+
+//Convertion Local to Goble
+//class outer{
+//    int g;
+//    String j;
+//    void get(int n,String name){
+//        g=n;
+//        j=name;
 //    }
 //    void set(){
-//        System.out.println("Hello");
+//        System.out.println(j+" "+g);
 //    }
 //}
-class demo{
-    void get(){
-        System.out.println("Get");
+
+
+//Return datatype
+class return_type {
+    int set(int a) {
+        return a;
     }
-    void get(int a){
-        System.out.println("int");
+
+    float set1(float a) {
+        return a;
     }
-    void get(float b){
-        System.out.println("float");
+
+    String set2(String a) {
+        return a;
+    }
+
+    byte set3(byte a) {
+        return (byte)a;
+    }
+
+    char set4(char ch) {
+        return ch;
+    }
+
+    short set8(int a) {
+        return (short)a;
+    }
+
+    long set5(long a) {
+        return a;
+    }
+
+    double set6(double a) {
+        return a;
+    }
+
+    boolean set7(boolean a) {
+        return a;
     }
 }
+public class oop_concept_1 {
+    public static void main(String[] args){
+    return_type t1=new return_type();
+    System.out.println("Int: "+t1.set(10));
+        System.out.println("float: "+t1.set1(10.2f));
+        System.out.println("String: "+t1.set2("String"));
+//        System.out.println("byte: "+t1.set3(10));
+        System.out.println("char: "+t1.set4('J'));
+        System.out.println( "long: "+t1.set5(10));
+        System.out.println( "short: "+t1.set8(11));
+        System.out.println("double: "+t1.set6(20.55555555555555));
+        System.out.println("boolen: "+t1.set7(true));
+
+
+
+    }
+}
+
 
